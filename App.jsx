f9ee506d4ea9726dbb1ee88f32a95194208cbf77
Jacob Miller <email>
2024-08-06 19:30:30 -0300

    fix statusbar text color not persisting on reload app and changing screens

--- App.jsx
+++ App.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./src/store";
 import AppNavigator from "./src/navigation/AppNavigator";
-import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <StatusBar
-        style="light"
-        translucent={true}
-        backgroundColor="#00000000"
-      />
       <AppNavigator />
     </Provider>
   );
