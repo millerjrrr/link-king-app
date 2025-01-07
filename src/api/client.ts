d35f8b93e6669c29918296ba269569729b74344a
Jacob Miller <email>
2025-01-07 10:47:20 -0300

    updated client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,13 +1,12 @@
 import axios, { AxiosInstance } from "axios";
 
-// need to find a better way to do this
-const client: { f: AxiosInstance; l: AxiosInstance } = {
-  f: axios.create({
-    baseURL: "http://192.168.1.69:3001",
-  }),
-  l: axios.create({
-    baseURL: "https://www.linkoking.com",
-  }),
-};
+const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://www.linkoking.com" // Production server
+    : "http://192.168.1.69:3001"; // Development server
 
-export default client.f;
+const client: AxiosInstance = axios.create({
+  baseURL,
+});
+
+export default client;
