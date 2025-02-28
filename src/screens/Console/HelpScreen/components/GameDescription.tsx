b170c7766781ce5d27ad100795ecc0625bb15789
Jacob Miller <email>
2025-02-28 12:22:58 -0300

    updated GameDescription to tsx

--- src/screens/Console/HelpScreen/components/GameDescription.tsx
+++ src/screens/Console/HelpScreen/components/GameDescription.tsx
@@ -1,19 +1,15 @@
 import { TouchableOpacity, Linking } from "react-native";
 import DescriptionWrapper from "./DescriptionWrapper";
 import { AntDesign } from "@expo/vector-icons";
-import { useSelector } from "react-redux";
-import { settingsState } from "@src/store/settings";
-import colors from "@src/utils/colors";
 import useCatchAsync from "@src/hooks/utilityHooks/useCatchAsync";
+import useColors from "@src/hooks/utilityHooks/useColors";
 
 const GameDescription = () => {
-  const { colorScheme, golden, appLang } =
-    useSelector(settingsState);
-  const color = colors[colorScheme].CONTRAST[golden];
+  const { CONTRAST } = useColors();
 
   const catchAsync = useCatchAsync();
 
-  const onPress = catchAsync(async () => {
+  const goToYoutube = catchAsync(async () => {
     const url =
       "https://www.youtube.com/@linkkingapp/shorts";
     const canOpen = await Linking.canOpenURL(url);
@@ -25,14 +21,14 @@ const GameDescription = () => {
   });
 
   return (
-    <DescriptionWrapper {...{ name: "gameDescription" }}>
-      {appLang === "pt" ? (
-        <TouchableOpacity {...{ onPress }}>
-          <AntDesign
-            {...{ name: "youtube", size: 96, color }}
-          />
-        </TouchableOpacity>
-      ) : null}
+    <DescriptionWrapper name="gameDescription">
+      <TouchableOpacity onPress={goToYoutube}>
+        <AntDesign
+          name="youtube"
+          size={96}
+          color={CONTRAST}
+        />
+      </TouchableOpacity>
     </DescriptionWrapper>
   );
 };
