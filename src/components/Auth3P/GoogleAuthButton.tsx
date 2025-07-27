99615e30c529f419e0916c3dee02cf1dfc7d5d39
Jacob Miller <millerjr@tcd.ie>
2025-07-27 19:51:02 -0300

    have loader show when logging in (important for slow android devices)

--- src/components/Auth3P/GoogleAuthButton.tsx
+++ src/components/Auth3P/GoogleAuthButton.tsx
@@ -3,7 +3,7 @@ import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
 import { makeRedirectUri } from "expo-auth-session";
 import client from "@src/api/client";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { settingsState } from "@src/store/settings";
 import useCatchAsync from "../../hooks/utilityHooks/useCatchAsync";
 import appTextSource from "@src/utils/appTextSource";
@@ -12,6 +12,7 @@ import Constants from "expo-constants";
 import Auth3PButton from "./Auth3PButton";
 import useUpdateAuthData from "@src/hooks/authHooks/useUpdateAuthData";
 import { Platform } from "react-native";
+import { updateBusyState } from "../../store/auth";
 
 type ExtraProps = {
   GOOGLE_WEB_CLIENT_ID?: string;
@@ -63,40 +64,56 @@ const GoogleAuthButton = () => {
 
   const catchAsync = useCatchAsync();
   const updateAuthData = useUpdateAuthData();
+  const dispatch = useDispatch();
 
   const loginWithGoogleToken = catchAsync(
     async (idToken: string): Promise<void> => {
-      if (
-        GOOGLE_WEB_CLIENT_ID === "" ||
-        GOOGLE_IOS_CLIENT_ID === "" ||
-        GOOGLE_ANDROID_CLIENT_ID === ""
-      )
-        throw new Error(
-          "Dev Error: Environment variables have not been set correctly",
-        );
-      const { data } = await client.post(
-        "/api/v1/users/sign-in-with-google",
-        {
-          idToken,
-        },
-        {
-          timeout: 3000,
-          headers: {
-            "Content-Type": "application/json",
-            "Accept-Language": appLang,
+      try {
+        dispatch(updateBusyState(true));
+        if (
+          GOOGLE_WEB_CLIENT_ID === "" ||
+          GOOGLE_IOS_CLIENT_ID === "" ||
+          GOOGLE_ANDROID_CLIENT_ID === ""
+        )
+          throw new Error(
+            "Dev Error: Environment variables have not been set correctly",
+          );
+        const { data } = await client.post(
+          "/api/v1/users/sign-in-with-google",
+          {
+            idToken,
           },
-        },
-      );
+          {
+            timeout: 3000,
+            headers: {
+              "Content-Type": "application/json",
+              "Accept-Language": appLang,
+            },
+          },
+        );
 
-      if (data.status === "success") updateAuthData(data);
+        if (data.status === "success")
+          await updateAuthData(data);
+      } finally {
+        dispatch(updateBusyState(false));
+      }
     },
   );
+
+  const promptAsyncWithBusy = () => {
+    try {
+      dispatch(updateBusyState(true));
+      promptAsync();
+    } finally {
+      dispatch(updateBusyState(false));
+    }
+  };
 
   return (
     <Auth3PButton
       name="google"
       title={continueWithGoogle}
-      onPress={() => promptAsync()}
+      onPress={promptAsyncWithBusy}
       disabled={!request}
       color="white"
       textColor="gray"
--- src/screens/auth/SignUp/Welcome.jsx
+++ src/screens/auth/SignUp/Welcome.jsx
@@ -8,13 +8,15 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "@src/utils/colors";
 import SignUpAppLink from "@components/SignUpAppLink";
 import AuthFormContainer from "@src/components/Containers/AuthFormContainer";
-import GoogleAuthButton from "@src/components/Auth3P/GoogleAuthButton";
 import Auth3PButtons from "@src/components/Auth3P/Auth3PButtons";
+import BusyWrapper from "../../../components/Loader/BusyWrapper";
+import { authState } from "../../../store/auth";
 
 const Welcome = () => {
   const { colorScheme, golden, appLang } =
     useSelector(settingsState);
   const color = colors[colorScheme].CONTRAST[golden];
+  const { busy } = useSelector(authState);
 
   const navigation = useNavigation();
 
@@ -31,18 +33,20 @@ const Welcome = () => {
       subHeading={subHeading}
       back={false}
     >
-      <MaterialCommunityIcons
-        name={"account-arrow-right-outline"}
-        size={100}
-        color={color}
-      />
-      <View style={{ height: 20 }} />
-      <AuthButton
-        title={buttonTitle}
-        busy={false}
-        onPress={onPress}
-      />
-      <SignUpAppLink />
+      <BusyWrapper busy={busy} size={150}>
+        <MaterialCommunityIcons
+          name={"account-arrow-right-outline"}
+          size={100}
+          color={color}
+        />
+        <View style={{ height: 20 }} />
+        <AuthButton
+          title={buttonTitle}
+          busy={false}
+          onPress={onPress}
+        />
+        <SignUpAppLink />
+      </BusyWrapper>
       <Auth3PButtons />
     </AuthFormContainer>
   );
